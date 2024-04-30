8e20f0c5ca80c7922db53a46846121e8f6a20235
Majdi Ben Youssef <benyoussef.mohamed@esprit.tn>
2024-05-01 00:56:40 +0100

    first commit

---
+++ src/main/java/missionentreprise/projetme/ProjetMeApplication.java
@@ -0,0 +1,13 @@
+package missionentreprise.projetme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/missionentreprise/projetme/ProjetMeApplicationTests.java
@@ -0,0 +1,13 @@
+package missionentreprise.projetme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
